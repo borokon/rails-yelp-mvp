aa6c41b8bf64a245850ef52fc25b94d099a06fdd
Koni Borowiak <borkoni123@yahoo.co.uk>
2024-05-01 12:59:34 +0100

    restaurants seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+categories = %w[chinese italian japanese french belgian]
+
+5.times do
+  Restaurant.create(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      category: categories.sample, # Randomly selects one category
+      phone_number: Faker::PhoneNumber.phone_number # Generates a random phone number
+  )
+end
